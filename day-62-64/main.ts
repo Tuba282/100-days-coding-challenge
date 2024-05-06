ce0193b07ada8575c5f62e8f5e0148cbfbe26195
Tuba Jan <121048565+Tuba282@users.noreply.github.com>
2024-05-07 04:34:58 +0500

    its my day 62 to 64 of 100 days coding challenge

---
+++ day-62-64/main.ts
@@ -0,0 +1,74 @@
+console.log(`\t\tIts my day 62  \n\n`);
+
+// Type Annotations
+
+// 1. Basic Types
+let my_name = "Abdul";
+console.log("my name is :", my_name, "\n"); //two different methods
+console.log(`my name is : ${my_name} \n`);
+
+// 2. Arrays
+let array_1 = [`Ali`, "Asad", `Ahmed`, "Muskan"];
+console.log(array_1); //it will print array_1 in simple array format
+console.log(array_1.slice(2).toString()); //it will print Ahmed
+let array_2 = ["Ali", 1, "Asad", 2, "Ahmed", 3, "Muskan"];
+console.log(array_2.filter((plant) => typeof plant === "number")); //it wil print only numbers from array_2
+
+// 3. Objects
+let my_obj = {
+  name: "Abdul",
+  age: "45",
+  job: false,
+  friends: {
+    school: [`Ahmed`],
+    college: [`Ali`],
+    now: [`No-One`],
+  },
+};
+//let print our object
+console.log(
+  `hey buddies my name is ${my_obj.name} and I am ${my_obj.age} Now ${my_obj.friends.now[0]} is
+   friend but in old days I uesed to have friend ${my_obj.friends.school[0]} in college and
+    ${my_obj.friends.school[0]} in school but being alone a really a good vibe.\n`
+);
+
+// 3. Tuples
+let make_tuple_1 = [`Ali`, 24, true, null, undefined];
+console.log(make_tuple_1[2]); //it will print "true" from tuple
+let make_tuple_2: [
+  name: string,
+  age: number,
+  married: boolean,
+  edc: null,
+  status: undefined
+] = [`Ali`, 24, true, null, undefined];
+
+//this is how we can make tuple multiple data declaration in one array.
+
+// 5. Any
+
+function dynamic(full_name: string): any {
+  return `hey his name is ${full_name}`;
+}
+console.log(dynamic(`Ali`));
+
+// 6. Void
+function using_void(): void {
+  console.log(`I am using void function`);
+}
+using_void();
+
+// 7. Null and Undefined
+function using_null_and_undefined(num: null, city:string="karachi") {
+  if (num === null) {
+    console.log(`yes our num is null`);
+  }
+  if (city === undefined) {
+    console.log(`yes our city is undefined`);
+    
+  }
+}
+using_null_and_undefined(null, `Lahore`);
+using_null_and_undefined(null, undefined);
+
+
